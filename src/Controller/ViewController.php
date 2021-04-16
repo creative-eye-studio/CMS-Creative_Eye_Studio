0def736e763f8d851aa1f57a69f61f5e209379b0
Kev <rifakev@gmail.com>
2021-04-16 22:40:33 +0200

    [Front-Back] - Gestion du menu de navigation

--- src/Controller/ViewController.php
+++ src/Controller/ViewController.php
@@ -14,8 +14,12 @@ class ViewController extends AbstractController
      */
     public function index(): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $linksNav = $entityManager->getRepository(Pages::class)->findAll();
+
         return $this->render('base.html.twig', [
             'controller_name' => 'ViewController',
+            'links' => $linksNav,
         ]);
     }
 
@@ -25,10 +29,16 @@ class ViewController extends AbstractController
     public function page_view(String $slug)
     {
         $entityManager = $this->getDoctrine()->getManager();
+        $linksNav = $entityManager->getRepository(Pages::class)->findAll();
         $page = $entityManager->getRepository(Pages::class)->findOneBy(['slug' => $slug]);
+
+        if($slug == "index"){
+            return $this->redirectToRoute('home');
+        }
 
         return $this->render('base.html.twig', [
             'controller_name' => 'ViewController',
+            'links' => $linksNav,
             'slugs' => $page,
         ]);
     }
--- var/cache/dev/twig/30/30c81b21666286768018129b6f3e0091800d36ff3a7f6cc739ba91fce6ec8899.php
+++ var/cache/dev/twig/30/30c81b21666286768018129b6f3e0091800d36ff3a7f6cc739ba91fce6ec8899.php
@@ -67,18 +67,32 @@ class __TwigTemplate_75904c184d759504a069e0ab2a7e8b24186374f258723d946c47e1d9d07
         <header></header>
         <nav>
             <ul>
-                <li>
+                ";
+        // line 20
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["links"]) || array_key_exists("links", $context) ? $context["links"] : (function () { throw new RuntimeError('Variable "links" does not exist.', 20, $this->source); })()));
+        foreach ($context['_seq'] as $context["_key"] => $context["link"]) {
+            // line 21
+            echo "                <li>
                     <a href=";
-        // line 21
-        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("home");
-        echo ">Accueil</a>
+            // line 22
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("page_view", ["slug" => twig_get_attribute($this->env, $this->source, $context["link"], "slug", [], "any", false, false, false, 22)]), "html", null, true);
+            echo ">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["link"], "name", [], "any", false, false, false, 22), "html", null, true);
+            echo "</a>
                 </li>
-            </ul>
+                ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['link'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 25
+        echo "            </ul>
         </nav>
         ";
-        // line 25
+        // line 27
         $this->displayBlock('body', $context, $blocks);
-        // line 32
+        // line 34
         echo "    </body>
 </html>
 ";
@@ -155,7 +169,7 @@ class __TwigTemplate_75904c184d759504a069e0ab2a7e8b24186374f258723d946c47e1d9d07
 
     }
 
-    // line 25
+    // line 27
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -165,22 +179,22 @@ class __TwigTemplate_75904c184d759504a069e0ab2a7e8b24186374f258723d946c47e1d9d07
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
-        // line 26
+        // line 28
         echo "            ";
-        if ((0 === twig_compare(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 26, $this->source); })()), "request", [], "any", false, false, false, 26), "attributes", [], "any", false, false, false, 26), "get", [0 => "_route"], "method", false, false, false, 26), "home"))) {
-            // line 27
-            echo "                ";
-            $this->loadTemplate("front/website/index.html.twig", "base.html.twig", 27)->display($context);
-            // line 28
-            echo "            ";
-        } elseif ((0 !== twig_compare(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 28, $this->source); })()), "request", [], "any", false, false, false, 28), "attributes", [], "any", false, false, false, 28), "get", [0 => "_route"], "method", false, false, false, 28), "home"))) {
+        if ((0 === twig_compare(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 28, $this->source); })()), "request", [], "any", false, false, false, 28), "attributes", [], "any", false, false, false, 28), "get", [0 => "_route"], "method", false, false, false, 28), "home"))) {
             // line 29
             echo "                ";
-            $this->loadTemplate((("front/website/" . twig_get_attribute($this->env, $this->source, (isset($context["slugs"]) || array_key_exists("slugs", $context) ? $context["slugs"] : (function () { throw new RuntimeError('Variable "slugs" does not exist.', 29, $this->source); })()), "slug", [], "any", false, false, false, 29)) . ".html.twig"), "base.html.twig", 29)->display($context);
+            $this->loadTemplate("front/website/index.html.twig", "base.html.twig", 29)->display($context);
             // line 30
             echo "            ";
+        } elseif ((0 !== twig_compare(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 30, $this->source); })()), "request", [], "any", false, false, false, 30), "attributes", [], "any", false, false, false, 30), "get", [0 => "_route"], "method", false, false, false, 30), "home"))) {
+            // line 31
+            echo "                ";
+            $this->loadTemplate((("front/website/" . twig_get_attribute($this->env, $this->source, (isset($context["slugs"]) || array_key_exists("slugs", $context) ? $context["slugs"] : (function () { throw new RuntimeError('Variable "slugs" does not exist.', 31, $this->source); })()), "slug", [], "any", false, false, false, 31)) . ".html.twig"), "base.html.twig", 31)->display($context);
+            // line 32
+            echo "            ";
         }
-        // line 31
+        // line 33
         echo "        ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
@@ -202,7 +216,7 @@ class __TwigTemplate_75904c184d759504a069e0ab2a7e8b24186374f258723d946c47e1d9d07
 
     public function getDebugInfo()
     {
-        return array (  184 => 31,  181 => 30,  178 => 29,  175 => 28,  172 => 27,  169 => 26,  159 => 25,  146 => 13,  136 => 12,  123 => 9,  113 => 8,  94 => 5,  82 => 32,  80 => 25,  73 => 21,  65 => 15,  63 => 12,  60 => 11,  57 => 8,  53 => 5,  47 => 1,);
+        return array (  198 => 33,  195 => 32,  192 => 31,  189 => 30,  186 => 29,  183 => 28,  173 => 27,  160 => 13,  150 => 12,  137 => 9,  127 => 8,  108 => 5,  96 => 34,  94 => 27,  90 => 25,  79 => 22,  76 => 21,  72 => 20,  65 => 15,  63 => 12,  60 => 11,  57 => 8,  53 => 5,  47 => 1,);
     }
 
     public function getSourceContext()
@@ -226,9 +240,11 @@ class __TwigTemplate_75904c184d759504a069e0ab2a7e8b24186374f258723d946c47e1d9d07
         <header></header>
         <nav>
             <ul>
+                {% for link in links %}
                 <li>
-                    <a href={{ path('home') }}>Accueil</a>
+                    <a href={{ path('page_view', { slug: link.slug }) }}>{{link.name}}</a>
                 </li>
+                {% endfor %}
             </ul>
         </nav>
         {% block body %}
