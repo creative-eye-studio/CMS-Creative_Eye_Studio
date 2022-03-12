96cb3aeb0de55cb87ef128e92600c1257f2a6dde
Kévin Rifa <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-03-12 12:15:40 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -41,36 +41,6 @@ CREATE TABLE `doctrine_migration_versions` (
   `execution_time` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `doctrine_migration_versions`
---
-
-INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20210413102550', '2021-04-13 10:49:51', 50),
-('DoctrineMigrations\\Version20210413104933', '2021-04-13 10:51:41', 53),
-('DoctrineMigrations\\Version20210413111327', '2021-04-13 11:13:37', 57),
-('DoctrineMigrations\\Version20210413155502', '2021-04-13 15:55:11', 125),
-('DoctrineMigrations\\Version20210414081038', '2021-04-14 08:10:47', 89),
-('DoctrineMigrations\\Version20210414182732', '2021-04-14 18:27:41', 80),
-('DoctrineMigrations\\Version20210416164256', '2021-04-16 16:43:06', 167),
-('DoctrineMigrations\\Version20210416205847', '2021-04-16 20:58:55', 135),
-('DoctrineMigrations\\Version20210417071815', '2021-04-17 07:18:23', 141),
-('DoctrineMigrations\\Version20210418113655', '2021-05-13 10:06:05', 252),
-('DoctrineMigrations\\Version20210513080552', '2021-05-13 10:06:58', 120),
-('DoctrineMigrations\\Version20210515094519', '2021-05-15 11:45:39', 131),
-('DoctrineMigrations\\Version20210715203800', '2021-07-15 22:38:11', 150),
-('DoctrineMigrations\\Version20210716044626', '2021-07-16 06:46:56', 79),
-('DoctrineMigrations\\Version20210716083156', '2021-07-16 10:32:05', 354),
-('DoctrineMigrations\\Version20210716083433', '2021-07-16 10:34:42', 180),
-('DoctrineMigrations\\Version20210716084038', '2021-07-16 10:41:03', 184),
-('DoctrineMigrations\\Version20210716084407', '2021-07-16 10:44:17', 196),
-('DoctrineMigrations\\Version20210716084551', '2021-07-16 10:46:00', 105),
-('DoctrineMigrations\\Version20210716084733', '2021-07-16 10:47:43', 89),
-('DoctrineMigrations\\Version20210716092046', '2021-07-16 11:20:56', 145),
-('DoctrineMigrations\\Version20210716092426', '2021-07-16 11:24:42', 240),
-('DoctrineMigrations\\Version20210911114649', '2021-09-11 13:48:01', 258),
-('DoctrineMigrations\\Version20210913132126', '2021-09-13 15:21:41', 150);
-
 -- --------------------------------------------------------
 
 --
