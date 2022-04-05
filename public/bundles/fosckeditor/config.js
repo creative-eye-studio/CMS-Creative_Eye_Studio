0517f2e3ced74ac84ceb5244e2b19b985ba02aa9
Kévin Rifa <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-04-05 19:00:18 +0200

    Revert "KR - Mise à jour des fichiers"
    This reverts commit 30094fd6ba8c03fe2b2b822f7fbd993caf2604f1.

--- public/bundles/fosckeditor/config.js
+++ public/bundles/fosckeditor/config.js
@@ -7,7 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.extraPlugins = 'codemirror,image2';
+	config.extraPlugins = 'codemirror,image2,imageresponsive';
 	config.allowedContent = true;
 	config.startupMode = 'source';
 };
